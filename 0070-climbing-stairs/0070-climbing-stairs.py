fbca14c18f69ce6388ffbb8c564313f539c94c4d
Joon <dr0joon@gmail.com>
2023-04-02 22:06:53 +0900

    Time: 31 ms (63.74%), Space: 13.7 MB (93.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,5 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
         mem = 1, 0 # 1로 끝나는 것, 2로 끝나는 것
         for _ in range(n):
             mem = mem[0] + mem[1], mem[0]
