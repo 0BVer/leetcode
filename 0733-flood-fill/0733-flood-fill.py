bbf12e749802b4948d3e34aa789477342fff9f72
Joon <dr0joon@gmail.com>
2023-04-08 02:45:11 +0900

    Time: 86 ms (12.96%), Space: 14.1 MB (58.66%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,12 +5,12 @@ class Solution:
         way = [0, 1], [1, 0], [0, -1], [-1, 0]
         if target == color: return image
         def dfs(y: int, x: int):
-            if image[y][x] != target: return
-            image[y][x] = color
-            
-            for i, j in way:
-                if 0 <= x + i < l_x and 0 <= y + j < l_y:
-                    dfs(y + j, x + i)
+            if image[y][x] == target:
+                image[y][x] = color
+
+                for i, j in way:
+                    if 0 <= x + i < l_x and 0 <= y + j < l_y:
+                        dfs(y + j, x + i)
             
         dfs(sr, sc)
         return image
